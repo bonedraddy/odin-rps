fd5629aebda917f2bf2c849271c01453a3415a1f
JD <bonedraddy@gmail.com>
2026-05-20 12:06:35 -0400

    Capture player and system choices. Display in paragraph.

--- rps-script.js
+++ rps-script.js
@@ -24,9 +24,68 @@ const gameState = {
 };
 
 //=============================
+// GAME PLAY
+//=============================
+/*const rockBtn = document.querySelector("#r");
+rockBtn.addEventListener("click", (event) => {
+  let playerInput = event.target.id;
+  gameState.playerChoice = playerInput;
+  console.log(gameState.playerChoice);
+});*/
+
+const gameChoices = document.querySelector("#selections");
+const playBtn = document.querySelector("#play-btn");
+const playerChoiceSpan = document.querySelector("#player-display");
+const systemChoiceSpan = document.querySelector("#system-display");
+gameChoices.addEventListener("click", logPlayerChoice);
+playBtn.addEventListener("click", generateSystemChoice);
+//let gameChoices = document.querySelector("#selections");
+//gameState.playerChoice = gameChoices.addEventListener("click", function (e) {
+//  console.log(e.target);
+//});
+
+//console.log(gameState.playerChoice);
+//let displayPlayerChoice = document.getElementById("player-display");
+//displayPlayerChoice.textContent = choiceDisplay.get(gameState.playerChoice);
+//let playBtn = document.querySelector("#play-btn");
+//playBtn.addEventListener("click", systemChoiceGenerator);
+
+//=============================
 // FUNCTIONS
 //=============================
-function systemChoiceGenerator() {
+
+function generateSystemChoice(event) {
   const options = ["r", "p", "s"];
-  return options[Math.floor(Math.random() * options.length)];
+  let systemInput = options[Math.floor(Math.random() * options.length)];
+  gameState.systemChoice = systemInput;
+  systemChoiceSpan.textContent = choiceDisplay.get(gameState.systemChoice);
+  console.log(gameState.systemChoice);
+}
+
+function logPlayerChoice(event) {
+  let playerInput = event.target.id;
+  gameState.playerChoice = playerInput;
+  playerChoiceSpan.textContent = choiceDisplay.get(gameState.playerChoice);
+  console.log(gameState.playerChoice);
+}
+
+function playRound(playerInput, systemInput) {
+  let outcome;
+  if (playerInput === systemInput) {
+    outcome = "t";
+    gameState.ties++;
+    return outcome;
+  } else if (
+    (playerInput === "r" && systemInput === "s") ||
+    (playerInput === "p" && systemInput === "r") ||
+    (playerInput === "s" && systemInput === "p")
+  ) {
+    outcome = "w";
+    gameState.wins++;
+    return outcome;
+  } else {
+    outcome = "l";
+    gameState.losses++;
+    return outcome;
+  }
 }
